aff0379d2bb786805830749e76269cbd1f9ee715
fin1te <rishabhmehta00@gmail.com>
2023-01-21 21:16:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/fin1te/hackoverflow/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fin1te.hackoverflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
