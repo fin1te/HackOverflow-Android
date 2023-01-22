f3577f904f33c982bc7ab6280cd40d442b75eabd
fin1te <rishabhmehta00@gmail.com>
2023-01-22 17:28:17 +0530

    Gradient CircleImageView implemented for ProfileFragment

--- app/src/main/java/com/fin1te/hackoverflow/ui/ProfileFragment.kt
+++ app/src/main/java/com/fin1te/hackoverflow/ui/ProfileFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.fin1te.hackoverflow.R
+import com.fin1te.hackoverflow.databinding.FragmentProfileBinding
+import de.hdodenhof.circleimageview.CircleImageView
 
 
 class ProfileFragment : Fragment() {
 
+    private  var binding : FragmentProfileBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+
+        val fragmentBinding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        Glide.with(this@ProfileFragment)
+            .load("https://xsgames.co/randomusers/assets/avatars/male/77.jpg")
+            .centerCrop()
+            .into(binding!!.profileImage)
+    }
 }
