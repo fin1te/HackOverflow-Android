ced153434f65df59918f102dcaf318ece2c6dc6b
fin1te <rishabhmehta00@gmail.com>
2023-01-25 05:45:46 +0530

    Implemented TimelineView for Offline & Online Fragments

--- app/src/main/java/com/fin1te/hackoverflow/model/OfflineEventRepo.kt
+++ app/src/main/java/com/fin1te/hackoverflow/model/OfflineEventRepo.kt
@@ -9,8 +9,33 @@ class OfflineEventRepo {
 
             eventList.add(Event("01-03-2023", "Booking Ends", "registrations closes"))
 
+            eventList.add(Event("16-03-2023", "Day 01", "11:00 - Reach Venue"))
+            eventList.add(Event("16-03-2023", "Day 01", "11:30 - Attendance"))
+            eventList.add(Event("16-03-2023", "Day 01", "11:30 - Get T-Shirt & ID "))
+            eventList.add(Event("16-03-2023", "Day 01", "13:00 - Lunch"))
+            eventList.add(Event("16-03-2023", "Day 01", "14:00 - Opening Ceremony"))
+            eventList.add(Event("16-03-2023", "Day 01", "16:00 - Lab Allotment"))
+            eventList.add(Event("16-03-2023", "Day 01", "17:00 - Coding Begins"))
+            eventList.add(Event("16-03-2023", "Day 01", "21:00 - Dinner"))
+            eventList.add(Event("16-03-2023", "Day 01", "22:00 - Networking"))
+            eventList.add(Event("16-03-2023", "Day 01", "23:00 - Coding"))
 
+            eventList.add(Event("17-03-2023", "Day 02", "08:00 - Breakfast"))
+            eventList.add(Event("17-03-2023", "Day 02", "09:00 - Coding"))
+            eventList.add(Event("17-03-2023", "Day 02", "13:00 - Lunch"))
+            eventList.add(Event("17-03-2023", "Day 02", "14:00 - Coding"))
+            eventList.add(Event("17-03-2023", "Day 02", "21:00 - Dinner"))
+            eventList.add(Event("17-03-2023", "Day 02", "22:00 - Networking"))
+            eventList.add(Event("17-03-2023", "Day 02", "23:00 - Coding"))
 
+            eventList.add(Event("18-03-2023", "Day 03", "07:00 - Project Submission"))
+            eventList.add(Event("18-03-2023", "Day 03", "08:00 - Breakfast"))
+            eventList.add(Event("18-03-2023", "Day 03", "09:00 - Judging"))
+            eventList.add(Event("18-03-2023", "Day 03", "11:30 - Winner Announcement"))
+            eventList.add(Event("18-03-2023", "Day 03", "11:30 - Closing Ceremony"))
+            eventList.add(Event("18-03-2023", "Day 03", "13:30 - Job Fair"))
+
+            eventList.add(Event("18-03-2023", "Conclusion", "Prizes for winners"))
 
             return eventList
         }
--- app/src/main/java/com/fin1te/hackoverflow/ui/TlOnlineFragment.kt
+++ app/src/main/java/com/fin1te/hackoverflow/ui/TlOnlineFragment.kt
@@ -5,16 +5,74 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.fin1te.hackoverflow.R
+import com.fin1te.hackoverflow.adapter.EventAdapter
+import com.fin1te.hackoverflow.databinding.FragmentTlOfflineBinding
+import com.fin1te.hackoverflow.databinding.FragmentTlOnlineBinding
+import com.fin1te.hackoverflow.model.Event
+import com.fin1te.hackoverflow.model.OnlineEventRepo
+import xyz.sangcomz.stickytimelineview.callback.SectionCallback
+import xyz.sangcomz.stickytimelineview.model.SectionInfo
 
 class TlOnlineFragment : Fragment() {
+
+    private var binding: FragmentTlOnlineBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tl_online, container, false)
+        val fragmentBinding = FragmentTlOnlineBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+        return fragmentBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initVerticalRecyclerView()
+    }
+
+    private fun initVerticalRecyclerView() {
+        val eventList = getEventList()
+        binding!!.onlineRecView.adapter = EventAdapter(
+            layoutInflater,
+            eventList,
+            R.layout.recycler_vertical_row
+        )
+
+        //Currently only LinearLayoutManager is supported.
+        binding!!.onlineRecView.layoutManager = LinearLayoutManager(
+            requireContext(),
+            RecyclerView.VERTICAL,
+            false
+        )
+
+        binding!!.onlineRecView.apply {
+            addItemDecoration(getSectionCallback(eventList))
+        }
+
+    }
+
+    //Get data method
+    private fun getEventList(): List<Event> = OnlineEventRepo().eventList
+
+
+    //Get SectionCallback method
+    private fun getSectionCallback(eventList: List<Event>): SectionCallback {
+        return object : SectionCallback {
+            //In your data, implement a method to determine if this is a section.
+            override fun isSection(position: Int): Boolean =
+                eventList[position].title != eventList[position - 1].title
+
+            //Implement a method that returns a SectionHeader.
+            override fun getSectionHeader(position: Int): SectionInfo {
+                val event = eventList[position]
+                return SectionInfo(event.title, event.subtitle)
+            }
+
+        }
+    }
 }
