a17d79c492cc9048d61ab752e53de8af9eeb3e4a
fin1te <rishabhmehta00@gmail.com>
2023-01-29 06:35:09 +0530

    Added LottieAnimations and TypeWriter TextAnimations

--- app/src/main/java/com/fin1te/hackoverflow/ui/OfflineNoticeFragment.kt
+++ app/src/main/java/com/fin1te/hackoverflow/ui/OfflineNoticeFragment.kt
@@ -1,10 +1,13 @@
 package com.fin1te.hackoverflow.ui
 
+import android.animation.ValueAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.fin1te.hackoverflow.R
 import com.fin1te.hackoverflow.databinding.FragmentOfflineNoticeBinding
 
 
@@ -25,8 +28,35 @@ class OfflineNoticeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        val noNoticeArray = requireContext().resources.getStringArray(R.array.noNoticeArray)
+        val noNoticeText = noNoticeArray.random()
+        typewriterAnimation(binding.noNoticeText, noNoticeText)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.noNoticeText.text = ""
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    private fun typewriterAnimation(textView: TextView, text: String) {
+        val textLength = text.length
+        val animationInterval = 75L // interval between each letter being added
+
+        textView.text = "" // clear the TextView
+        val textAnimator = ValueAnimator.ofInt(0, textLength)
+        textAnimator.duration = (textLength * animationInterval).toLong() // duration of animation is determined by text length
+        textAnimator.addUpdateListener { valueAnimator ->
+            val textIndex = valueAnimator.animatedValue as Int
+            textView.text = text.substring(0, textIndex)
+        }
+        textAnimator.start()
     }
 }
