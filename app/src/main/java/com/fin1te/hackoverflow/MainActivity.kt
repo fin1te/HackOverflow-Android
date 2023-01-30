fb6886da809db9abc0bc87f36ba1949f7f58d897
fin1te <rishabhmehta00@gmail.com>
2023-01-30 08:40:46 +0530

    Added Force App Update functionality and Custom Logout Dialog

--- app/src/main/java/com/fin1te/hackoverflow/MainActivity.kt
+++ app/src/main/java/com/fin1te/hackoverflow/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.fin1te.hackoverflow
 
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.cardview.widget.CardView
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.fin1te.hackoverflow.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
 
         //DataBinding
@@ -26,9 +37,79 @@ class MainActivity : AppCompatActivity() {
 
         navController = navHostFragment.navController
         binding.bottomNavigation.setupWithNavController(navController)
+
+        checkForUpdate()
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+    private fun checkForUpdate() {
+        Log.d("checkUpdate", "checkForUpdate: Called")
+        val database = Firebase.database.reference
+        database.child("versionCode").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                Log.d("checkUpdate", "onDataChange: Called")
+                val latestVersion = dataSnapshot.getValue(String::class.java)
+                val manager = this@MainActivity.packageManager
+                val info = manager.getPackageInfo(this@MainActivity.packageName, 0)
+                val currentVersion = info.versionName.toString()
+                Log.d("checkUpdate", "$latestVersion ; $currentVersion")
+
+                if (latestVersion != currentVersion) {
+                    val dialogView = layoutInflater.inflate(R.layout.dialog_update, null)
+                    val dialog = AlertDialog.Builder(this@MainActivity)
+                        .setCancelable(false)
+                        .setView(dialogView)
+                        .create()
+
+                    dialogView.findViewById<CardView>(R.id.btnUpdate).setOnClickListener {
+                        startActivity(
+                            Intent(
+                                Intent.ACTION_VIEW,
+                                Uri.parse("https://phcet.tech/app")
+                            )
+                        )
+                    }
+
+                    dialog.show()
+                }
+
+
+                // Show dialog to prompt user to update the app
+//                if (latestVersion != currentVersion) {
+//                    val dialog = AlertDialog.Builder(this@MainActivity)
+//                        .setCancelable(false)
+//                        .setTitle("Update Available")
+//                        .setMessage("Please update the app to the latest version to continue using the app")
+//                        .setPositiveButton("OK") { _, _ ->
+//                            startActivity(
+//                                Intent(
+//                                    Intent.ACTION_VIEW,
+//                                    Uri.parse("https://phcet.tech/app")
+//                                )
+//                            )
+//                        }.create()
+//                    dialog.show()
+//                            .setPositiveButton("OK") { _, _ ->
+//                                val appPackageName = packageName // get the package name
+//                                try {
+//                                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=$appPackageName")))
+//                                } catch (e: ActivityNotFoundException) {
+//                                    startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
+//                                }
+//                            }
+
+//                }
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handle error
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/fin1te/hackoverflow/ui/HomeFragment.kt
+++ app/src/main/java/com/fin1te/hackoverflow/ui/HomeFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.viewpager.widget.ViewPager
 import com.fin1te.hackoverflow.adapter.TimelinePagerAdapters
